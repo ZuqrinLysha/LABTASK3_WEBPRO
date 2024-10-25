3889333d7040e07a7ef123229e14430aeb29029c
AlipNuqman <alifnuqmann09@gmail.com>
2024-10-25 23:51:26 +0800

    siap

--- student_search.php
+++ student_search.php
@@ -3,63 +3,89 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title></title>
+  <title>Search Record</title>
 </head>
 <body>
   <center>
-  <p>
- 
-
-    
- 
-  <strong>Search Record</strong>  
-  <form action="covid_pbu_semak_student.php" method="get">
-    <p>Search by Name: 
-    <label for="nama"></label>
-    <input type="text" name="nama" id="nama" />
-    
-    <input type="submit" name="search" id="button2" value="Search" />
-    
-    <input type="hidden" name="nilai_search" id="hiddenField" value="1" />
-</form>
-
-  <form action="student_search.php" method="get">
-    <input type="submit" name="showAll" id="showAll" value="Show All" />
-    <input type="hidden" name="nilai_search" id="hiddenField" value="0" />
+    <strong>Search Record</strong>  
+    <form action="" method="get">
+      <p>Search by Name: 
+        <label for="nama"></label>
+        <input type="text" name="nama" id="nama" />
+        <input type="submit" name="search" id="button2" value="Search" />
+        <input type="hidden" name="nilai_search" value="1" />
+      </p>
     </form>
-  </p>
-  <table class="roundedTable" width="672" height="85" border="0" cellpadding="2" bgcolor="#FFFFFF">
-    <tr>
-    <td width="50" align="center" bgcolor="#FFFF00"><strong>No.</strong></td>
-    <td width="129" height="23" align="center" bgcolor="#FFFF00"><strong>Name</strong></td>
-    <td width="120" align="center" bgcolor="#FFFF00"><strong>
-      <label for="nama4">Matric Numb.</label>
-    </strong></td>
-    <td width="66" align="center" bgcolor="#FFFF00"><strong>Class</strong></td>
-    <td width="68" align="center" bgcolor="#FFFF00"><strong>Temp. </strong></td>
-    <td width="104" align="center" bgcolor="#FFFF00"><strong>Date</strong></td>
-  </tr>
 
+    <?php
+    // Database connection (update with your credentials)
+    $servername = "localhost";
+    $username = "root"; // Replace with your MySQL username
+    $password = ""; // Replace with your MySQL password
+    $dbname = "student_data";
 
-  <tr>
-    <td width="50" align="center">1</td>
-    <td width="129" height="23">WAN MOHD ZULHAFIRIE BIN WAN AHMAD</strong></td>
-    <td width="120" align="center">
-      <label for="nama4">21DDT19F1055</label></td>
-    <td width="66" align="center">DDT1E</td>
-    <td width="68" align="center">36 °C</td>
-    <td width="104" align="center">05 Nov 2021</td>
-  </tr>
-</table>
-<form action="main_menu.php" method="post">
-<p>
-  <input type="submit" name="button" id="button" value="Back" />
-</p>
-</form>
-</center>
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // If "Show All" button is clicked or a search query is made
+    if (isset($_GET['showAll']) || isset($_GET['search'])) {
+        // If "Search" is clicked, search by name
+        if (isset($_GET['nama']) && $_GET['nama'] != '') {
+            $name = $_GET['nama'];
+            $sql = "SELECT * FROM students WHERE name LIKE '%$name%'";
+        } else {
+            // If "Show All" is clicked, display all records
+            $sql = "SELECT * FROM students";
+        }
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo '<table class="roundedTable" width="672" border="0" cellpadding="2" bgcolor="#FFFFFF">
+                    <tr>
+                      <td width="50" align="center" bgcolor="#FFFF00"><strong>No.</strong></td>
+                      <td width="129" align="center" bgcolor="#FFFF00"><strong>Name</strong></td>
+                      <td width="120" align="center" bgcolor="#FFFF00"><strong>Matric Numb.</strong></td>
+                      <td width="66" align="center" bgcolor="#FFFF00"><strong>Class</strong></td>
+                      <td width="68" align="center" bgcolor="#FFFF00"><strong>Temp.</strong></td>
+                      <td width="104" align="center" bgcolor="#FFFF00"><strong>Date</strong></td>
+                    </tr>';
+            $no = 1;
+            while ($row = $result->fetch_assoc()) {
+                echo '<tr>
+                        <td align="center">' . $no . '</td>
+                        <td>' . $row["name"] . '</td>
+                        <td align="center">' . $row["matric_number"] . '</td>
+                        <td align="center">' . $row["class"] . '</td>
+                        <td align="center">' . $row["temperature"] . ' °C</td>
+                        <td align="center">' . $row["date"] . '</td>
+                      </tr>';
+                $no++;
+            }
+            echo '</table>';
+        } else {
+            echo "No records found.";
+        }
+    }
+
+    // Close connection
+    $conn->close();
+    ?>
+
+    <form action="" method="get">
+      <input type="submit" name="showAll" id="showAll" value="Show All" />
+    </form>
+
+    <form action="main_menu.php" method="post">
+      <p>
+        <input type="submit" name="button" id="button" value="Back" />
+      </p>
+    </form>
+  </center>
 </body>
 </html>
-
-
-
